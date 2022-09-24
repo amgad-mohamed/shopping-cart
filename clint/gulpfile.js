6dd220b628fe80fccce584dc37ea48241270857f
Amgad Mohamed <amgagmohamed38@gmail.com>
2022-09-24 23:43:41 +0200

    footer comp

--- clint/gulpfile.js
+++ clint/gulpfile.js
@@ -2,6 +2,9 @@ const glup = require("gulp");
 const glupSass = require("gulp-sass");
 const sass = glupSass(require("sass"));
 
-glup.task("sass", async function () {
-  glup.src("src/components/**/*.scss").pipe(sass()).pipe(glup.dest("src/css"));
-});
+glup.task("watch", async function(){
+  glup.watch("src/components/**/*.scss", async function(){
+    glup.src("src/components/**/*.scss").pipe(sass()).pipe(glup.dest("src/css"));
+
+  })
+})
---
+++ clint/src/components/Footer/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import "../../css/Footer/Footer.css"
+function Footer() {
+  return (
+    <footer>&copy; CopyWrite Amgad Mohamed 2022</footer>
+    )
+}
+
+export default Footer
