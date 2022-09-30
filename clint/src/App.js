efda2393a08bac747ccd69de357a0aad05cf4b28
Amgad Mohamed <amgagmohamed38@gmail.com>
2022-10-01 00:55:55 +0200

    filter compp

--- clint/src/App.js
+++ clint/src/App.js
@@ -1,11 +1,44 @@
 import React, { useState } from "react";
+import Filter from "./components/Filter/Filter";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import Products from "./components/Product/Products";
 import data from "./data.json";
 
 function App() {
-  const [products, ] = useState(data);
+  const [products, setProducts] = useState(data);
+  const [sort, setSort] = useState();
+  const [size, setSize] = useState();
+
+  const handleFilterBySize = (e) => {
+    setSize(e.target.value);
+    if (e.target.value === "ALL") {
+      setProducts(data);
+    } else {
+      const productClone = [...products];
+      const newProduct = productClone.filter(
+        (p) => p.sizes.indexOf(e.target.value) !== -1
+      );
+      setProducts(newProduct);
+    }
+  };
+  console.log(products);
+
+  const handleFilterBySort = (e) => {
+    const order = e.target.value;
+    setSort(order);
+    const productClone = [...products];
+    const newProduct = productClone.sort(function (a, b) {
+      if (order === "Lower") {
+        return a.price - b.price;
+      } else if (order === "Highst") {
+        return b.price - a.price;
+      } else {
+        return a.id < b.id ? 1 : -1;
+      }
+    });
+    setProducts(newProduct);
+  };
 
   return (
     <div className="layout">
@@ -13,7 +46,12 @@ function App() {
       <main>
         <div className="wrapper">
           <Products products={products} />
-          <div className="filter-wrapper">filter</div>
+          <Filter
+            handleFilterBySize={handleFilterBySize}
+            size={size}
+            handleFilterBySort={handleFilterBySort}
+            sort={sort}
+          />
         </div>
       </main>
 
---
+++ clint/src/components/Filter/Filter.js
@@ -0,0 +1,32 @@
+import React from "react";
+import "../../css/Filter/Filter.css";
+
+function Filter(props) {
+  return (
+    <div className="filter-wrapper">
+      <h2 className="filter-title">Filter</h2>
+      <p className="num-of-product">Number of products : 4 product</p>
+      <div className="filter-by-size">
+        <span>Filter</span>
+        <select className="fiter-select" onChange={props.handleFilterBySize} value = {props.size}>
+          <option value="ALL">ALL</option>
+          <option value="S">S</option>
+          <option value="M">M</option>
+          <option value="L">L</option>
+          <option value="XL">XL</option>
+          <option value="XXL">XXL</option>
+        </select>
+      </div>
+      <div className="filter-by-size">
+        <span>Order</span>
+        <select className="order-select" onChange={props.handleFilterBySort} value = {props.sort}>
+          <option value="Latest">Latest</option>
+          <option value="Lower">Lower</option>
+          <option value="Highst">Highst</option>
+        </select>
+      </div>
+    </div>
+  );
+}
+
+export default Filter;
