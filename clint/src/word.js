fef5d769390ffab7ea0617ef27aa116d78ed9ffa
Amgad Mohamed <amgagmohamed38@gmail.com>
2022-10-15 23:46:19 +0200

    server

--- clint/src/word.js
+++ clint/src/word.js
@@ -1,5 +1,5 @@
 export const words = {
-    headerTittle : "Shopping Cart",
-    mainTittle : "Content",
-    footerTittli: " © CopyWrite Amgad Mohamed 2022"
-}
+  headerTittle: "Shopping Cart",
+  mainTittle: "Content",
+  footerTittli: " © CopyWrite Amgad Mohamed 2022",
+};
