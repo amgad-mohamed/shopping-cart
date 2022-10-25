e0e8cdf1b60594c7280f35523ae3d9d940eff4f8
Amgad Mohamed <amgagmohamed38@gmail.com>
2022-10-26 00:13:14 +0200

    order redux2

--- clint/src/components/Cart/OrderModal.js
+++ clint/src/components/Cart/OrderModal.js
@@ -4,49 +4,47 @@ import Modal from "react-modal";
 function OrderModal(props) {
   const { order, closeModal, cartItems } = props;
   return (
-    <>
-      {order && (
-        <Modal isOpen={order} onRequestClose={closeModal}>
-          <div className="order-info">
-            <span className="close-icon" onClick={closeModal}>
-              &times;
-            </span>
-            <p className="alert-success">order done successfully</p>
-            <table>
-              <tr>
-                <td>Name:</td>
-                <td>{order.name}</td>
-              </tr>
-              <tr>
-                <td>Email:</td>
-                <td>{order.email}</td>
-              </tr>
-              <tr>
-                <td>Total:</td>
-                <td>
-                  $
-                  {cartItems.reduce((acc, p) => {
-                    return p.qty !== 0 ? acc + p.price * p.qty : null;
-                  }, 0)}
-                </td>
-              </tr>
-              <tr>
-                <td>Selected Items:</td>
-                <td>
-                  {cartItems.map((p) => (
-                    <div className="cart-data">
-                      <p>Number of this products: {p.qty}</p>
-                      <p>Title of products: {p.title}</p>
-                      <br />
-                    </div>
-                  ))}
-                </td>
-              </tr>
-            </table>
-          </div>
-        </Modal>
-      )}
-    </>
+  <>
+  {order && <Modal isOpen={order} onRequestClose={closeModal}>
+      <div className="order-info">
+        <span className="close-icon" onClick={closeModal}>
+          &times;
+        </span>
+        <p className="alert-success">order done success</p>
+        <table>
+          <tr>
+            <td>Name:</td>
+            <td>{order.name}</td>
+          </tr>
+          <tr>
+            <td>Email:</td>
+            <td>{order.email}</td>
+          </tr>
+          <tr>
+            <td>Total:</td>
+            <td>
+              ${" "}
+              {cartItems.reduce((acc, p) => {
+                return p.qty !== 0 ? acc + p.price * p.qty : null;
+              }, 0)}
+            </td>
+          </tr>
+          <tr>
+            <td>Select Product:</td>
+            <td>
+              {cartItems.map((p) => (
+                <div className="cart-data">
+                  <p>Number of this product: {p.qty}</p>
+                  <p>Title of product: {p.title}</p>
+                  <br />
+                </div>
+              ))}
+            </td>
+          </tr>
+        </table>
+      </div>
+    </Modal>}
+  </>
   );
 }
 
